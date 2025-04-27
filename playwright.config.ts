77e6cfc5bd603a98ff729dce44faa0f8f9b0d5ad
Hao Mai <maigiahao1209@gmail.com>
2025-04-27 21:11:09 +0700

    Init pw structure and config

---
+++ playwright.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig, devices } from '@playwright/test'
+
+export default defineConfig({
+    testDir: './tests',
+    projects: [
+        {
+            name: 'Chromium',
+            use: { ...devices['Desktop Chrome'] }
+        },
+        {
+            name: 'Webkit',
+            use: { ...devices['Desktop Safari'] }
+        }
+    ],
+    use: {
+        headless: false
+    }
+})
