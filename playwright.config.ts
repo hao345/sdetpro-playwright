4ce8f78f020a1b26d428198bca096e17e6f81ca1
Hao Mai <maigiahao1209@gmail.com>
2025-04-29 20:27:33 +0700

    add git ingore

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,13 +6,11 @@ export default defineConfig({
         {
             name: 'Chromium',
             use: { ...devices['Desktop Chrome'] }
-        },
-        {
-            name: 'Webkit',
-            use: { ...devices['Desktop Safari'] }
         }
     ],
     use: {
-        headless: false
+        headless: false,
+        baseURL: "https://the-internet.herokuapp.com/",
+        actionTimeout: 5000
     }
 })
